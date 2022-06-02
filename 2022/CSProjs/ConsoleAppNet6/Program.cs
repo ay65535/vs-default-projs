3ecda3483e468c3003806fc46a0fb18f3d380859
ay65535 <ay65535@icloud.com>
2022-06-03 00:40:26 +0900

    Refactor

--- 2022/CSProjs/ConsoleAppNet6/Program.cs
+++ 2022/CSProjs/ConsoleAppNet6/Program.cs
@@ -3,7 +3,7 @@
 using ConsoleAppNet6;
 using Newtonsoft.Json;
 
-Product product = new Product();
+Product product = new();
 product.Name = "Apple";
 product.Expiry = new DateTime(2008, 12, 28);
 product.Sizes = new string[] { "Small" };
